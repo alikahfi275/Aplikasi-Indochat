24ef9271ef54b118ad3989b0ffb74d11dcfa68d8
Ali Kahfi Lubis <alikahfi275@gmail.com>
2023-01-03 09:02:54 -0800

    Chat List Item

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import ChatListItem from "./src/components/ChatListItem";
+
+const chat = {
+  id: 1,
+  user: {
+    image:
+      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg",
+    name: "lukas",
+  },
+  lastMassage: {
+    text: "Oke",
+    createdAt: "07:30",
+  },
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ChatListItem chat={chat} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/components/ChatListItem/index.js
@@ -0,0 +1,62 @@
+import { View, Text, Image, StyleSheet } from "react-native";
+import React from "react";
+
+const ChatListItem = (props) => {
+  console.log("====================================");
+  console.log(props);
+  console.log("====================================");
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        }}
+        style={styles.image}
+      />
+      <View style={styles.content}>
+        <View style={styles.row}>
+          <Text numberOfLines={1} style={styles.name}>
+            William Lukas
+          </Text>
+          <Text style={styles.subTitle}>08:30</Text>
+        </View>
+        <Text numberOfLines={2} style={styles.subTitle}>
+          Hello There !!!
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default ChatListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    marginHorizontal: 10,
+    marginVertical: 5,
+    height: 70,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 10,
+  },
+  content: {
+    flex: 1,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: "lightgray",
+  },
+  row: {
+    flexDirection: "row",
+    marginBottom: 5,
+  },
+  name: {
+    flex: 1,
+    fontWeight: "bold",
+  },
+  subTitle: {
+    color: "grey",
+  },
+});
